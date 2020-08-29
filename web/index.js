f26c7fe28fb850bd602edc6fe6b0dfb97a6534fe
psycho-0609 <luuhung411@gmail.com>
2020-08-29 22:32:47 +0700

    e

--- web/index.js
+++ web/index.js
@@ -10,11 +10,15 @@ $(document).ready(function()
         $(el).click(function(){
             let height = $(animate).eq(index).css("height");
             height = height.replace("px","00");
+            let inew ="fa fa-chevron-up";
+            let iold = "fa fa-chevron-down";
             if(height>0){
                 $(animate).eq(index).css("height","0");
+                $(el).find("i").attr("class",iold);
             }
             else{
                 $(animate).eq(index).css("height","100%");
+                $(el).find("i").attr("class",inew);
             }
 
             console.log(height)
