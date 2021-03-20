181a0d6860dbfeb5d6ba6a3ca74fc9800d05d8d6
psycho-0609 <luuhung411@gmail.com>
2021-03-20 13:00:40 +0700

    update

--- Men-Shop/template/js/cart.js
+++ Men-Shop/template/js/cart.js
@@ -1,16 +1,80 @@
-$(document).ready(function(){
-    $(".btn-minus").on("click",function(){
-        let input = $(this).closest('div').find(".inputQuanlity")
-        if(parseInt(input.val())-1 > 0) {
-            input.val(parseInt(input.val()) -1)
+$(document).ready(function () {
+    $("#preTotal").text(totalPrice()+" ₫");
+    $("#totalCard").text(totalPrice()+" ₫");
+    $(".btn-minus").on("click",  function () {
+        let divContent = $(this).closest('div').closest('.cart-product-content');
+        let input = divContent.find(".product-quantity").find('.inputQuanlity');
+        let price = divContent.find(".product-price").find("#price");
+        
+        let fiPrice = price.text().replaceAll(".","") / input.val();
+        if (parseInt(input.val()) - 1 > 0) {
+            
+            input.val(parseInt(input.val()) - 1)
+
         }
-      
+        
+        price.text(processString(""+fiPrice * input.val()+""))
+        $("#preTotal").text(totalPrice()+" ₫");
+        $("#totalCard").text(totalPrice()+" ₫");
+       
+
     })
 
-    $(".btn-plus").on("click",function(){
-        let input = $(this).closest('div').find(".inputQuanlity")
-        input.val(parseInt(input.val()) +1)
-    })
+    $(".btn-plus").on("click", function () {
+        let divContent = $(this).closest('div').closest('.cart-product-content');
+        let input = divContent.find(".product-quantity").find('.inputQuanlity');
+        let price = divContent.find(".product-price").find("#price");
+        let fiPrice = price.text().replaceAll(".","") / input.val();
+        input.val(parseInt(input.val()) + 1);
+        price.text(processString(""+fiPrice * input.val()+""))
+        $("#preTotal").text(totalPrice()+" ₫");
+        $("#totalCard").text(totalPrice()+" ₫");
 
+    })
     
+    $(".btn-delete").on("click",function(e){
+        e.preventDefault();
+        let divBox = $(this).closest(".card-product-item");
+        divBox.remove();
+        if($(".btn-delete").length <= 0){
+            $(".empty-cart").css("display","block");
+            $(".btn-order").attr("disable","true");
+            console.log($(".btn-order"))
+        }
+        $("#preTotal").text(totalPrice()+" ₫");
+        $("#totalCard").text(totalPrice()+" ₫");
+    })
+
+    function totalPrice(){
+        let totalPrice = 0;
+        
+        $(".cart-product-content").map(function(){
+            let priceItemEl = $(this).find(".product-price").find("#price");
+            let price = priceItemEl.text().replaceAll(".","");
+            totalPrice += parseInt(price);
+        })
+        return processString(""+totalPrice+"");
+    }
+
+    function processString(string){
+        string = string.split("");
+        let result=[];
+        
+        for(let i = string.length-1; i >= 0; i--){
+            
+            
+            if((string.length - i) % 3 ==0 && i > 0){
+                
+                result += string[i] + ".";
+               
+            }else{
+                result += string[i];
+               
+            }
+        }
+        return result.split("").reverse().join("");;
+    }
+
+
+
 })
